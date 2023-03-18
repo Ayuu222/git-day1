38be4e8670558fee6501a20c44ff36fdde5820db
Ayuu222 <ayodhya.late173@gmail.com>
2023-03-18 15:08:57 +0530

    A.java change

--- A.java
+++ A.java
@@ -3,14 +3,21 @@ public class A{
 	{
 		System.out.println("Welcome");
 	}
+<<<<<<< Updated upstream
 	//for bug1
+=======
+
+>>>>>>> Stashed changes
 	public void m1()
 	{
 		System.out.println("hiii");
 	}
+<<<<<<< Updated upstream
      // for bug2
 	public void m2(){
 		System.out.println("Hello");
 	}
+=======
+>>>>>>> Stashed changes
 
 }
