7789d47414ececf93509df58a91f1f8bfc5c3dc9
Gayathri_Lakhena <gayatri.kalal2013@gmail.com>
2023-03-18 16:26:34 +0530

    Changes by Gayatri

--- B.java
+++ B.java
@@ -3,5 +3,9 @@ public static void main(String args[]){
 System.out.println("Hello i am Developer1");
   System.out.println("Hello i am Developer2");
 }
+public void gayatri()
+{
+ System.out.println("This is Gayatri");
+}
 
 }
