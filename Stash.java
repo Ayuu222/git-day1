34fed11141e83a1ff66f69cc193d8f0d8e5441bf
akankshadevansh <jadhav.akanksha1020@gmail.com>
2023-03-19 16:11:22 +0530

    stash file

--- Stash.java
+++ Stash.java
@@ -8,15 +8,13 @@
 stash
 ------------------------------------
 
-
-<<<<<<< Updated upstream
 public int m2()
 {
   return 10;
-=======
+
 public void m1()
 {
 System.out.println("method m1");
 
->>>>>>> Stashed changes
+
 }
